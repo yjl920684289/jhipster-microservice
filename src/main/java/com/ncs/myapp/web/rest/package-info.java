ca7d874fb18aa6ee06fa9acdea2fd2a6f415b082
yjl920684289 <junliangyuan@126.com>
2018-04-29 16:49:46 +0800

    Initial application generated by JHipster-5.0.0-beta.0

---
+++ src/main/java/com/ncs/myapp/web/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring MVC REST controllers.
+ */
+package com.ncs.myapp.web.rest;
---
+++ src/main/java/com/ncs/myapp/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.ncs.myapp.web.rest.vm;
